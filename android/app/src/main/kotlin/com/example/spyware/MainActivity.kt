1405b70a439e88caa0ebf277dcbc5c948377651d
BriLeighk <kirchgessner@wisc.edu>
2024-04-21 10:34:57 -0500

    Added permissions page for each app.

--- android/app/src/main/kotlin/com/example/spyware/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/spyware/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.spyware
-import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
 import android.content.pm.PackageManager
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import java.io.BufferedReader
@@ -11,42 +12,51 @@ import android.util.Base64
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
+import android.provider.Settings
 
 class MainActivity : FlutterActivity() {
     companion object {
         private const val CHANNEL = "samples.flutter.dev/spyware"
+        private const val SETTINGS_CHANNEL = "com.example.spyware/settings"
     }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
             .setMethodCallHandler { call, result ->
-                when (call.method) {
+                when (call.method) { //when the method getSpywareApps is called
                     "getSpywareApps" -> {
-                        val spywareApps = getDetectedSpywareApps()
-                        if (spywareApps != null && spywareApps.size != 0) {
+                        val spywareApps = getDetectedSpywareApps() //call the method to retrieve apps from device and do comparison
+                        if (spywareApps != null) {
                             result.success(spywareApps)
                         } else {
-                            result.error("UNAVAILABLE", "No spyware apps were detected on your device.", null)
+                            result.error("UNAVAILABLE", "Error retrieving list of apps from the device.", null)
                         }
                     }
                     else -> result.notImplemented()
                 }
             }
+            //ADD SETTINGS METHOD CHANNEL
+
     }
-
+    /*
+    * Method to retrieve apps on device and compare against database of spyware apps
+    */
     private fun getDetectedSpywareApps(): List<Map<String, Any>>? {
-        val apps = mutableListOf<Map<String, Any>>()
+        val apps = mutableListOf<Map<String, Any>>() //to store all app data from device
 
-        val infos = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-        val detectedSpywareApps = mutableListOf<Map<String, Any>>()
+        val infos = packageManager.getInstalledApplications(PackageManager.GET_META_DATA) //gets all app info
+        val detectedSpywareApps = mutableListOf<Map<String, Any>>() //to store all apps from device detected as spyware
 
-        infos.forEach { info ->
-            val appName = info.loadLabel(packageManager).toString()
-            val appID = info.packageName
-            val drawableIcon = info.loadIcon(packageManager)
+        infos.forEach { info -> //for each app and it's corresponding information:
+            val appName = info.loadLabel(packageManager).toString() //store its app name
+            val appID = info.packageName //store its unique app id
+            val permissions = packageManager.getPackageInfo(appID, PackageManager.GET_PERMISSIONS)
+            .requestedPermissions?.toList() ?: listOf<String>()
 
-            val iconBitmap = if (drawableIcon is BitmapDrawable) {
+            val drawableIcon = info.loadIcon(packageManager) //store its app icon bits
+
+            val iconBitmap = if (drawableIcon is BitmapDrawable) { //creates icon bitmap
                 drawableIcon.bitmap
             } else {
                 Bitmap.createBitmap(drawableIcon.intrinsicWidth, drawableIcon.intrinsicHeight, 
@@ -56,19 +66,26 @@ class MainActivity : FlutterActivity() {
                     drawableIcon.draw(canvas)
                 }
             }
-            val baos = ByteArrayOutputStream()
+            val baos = ByteArrayOutputStream() 
             iconBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
             val iconBytes = baos.toByteArray()
             val iconBase64 = Base64.encodeToString(iconBytes, Base64.NO_WRAP)
             
             Log.d("AppIconBase64", "Icon Base64 for $appName: $iconBase64")
-            apps.add(mapOf("id" to appID, "name" to appName, "icon" to iconBase64))
+            val appInfo = mapOf(
+                "id" to appID, 
+                "name" to appName, 
+                "icon" to iconBase64,
+                "permissions" to permissions
+                ) //adds app info to list of all apps from device
+            
+            apps.add(appInfo)
         }
 
         try {
             // Log before attempting to access file
             Log.d("SpywareDetection", "Attempting to open app-ids-research.csv")
-            assets.open("app-ids-research.csv").use { inputStream ->
+            assets.open("app-ids-research.csv").use { inputStream -> //opens the csv file
             BufferedReader(InputStreamReader(inputStream)).use { reader ->
                 reader.readLine() // Skip header line
                 var line: String?
@@ -76,7 +93,7 @@ class MainActivity : FlutterActivity() {
                     val tokens = line!!.split(",")
                     if (tokens.isNotEmpty()) {
 
-                        val csvAppId = tokens[0].trim() // Extrack app ID from current line in CSV file
+                        val csvAppId = tokens[0].trim() // Extract app ID from current line in CSV file
                         // Do comparison, see if app is downloaded on the device
                         val isInstalled = apps.any { app -> app["id"] == csvAppId}
                         if (isInstalled) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Test - Spyware Detector App', //title of app
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
+            //color scheme of entire app
             seedColor: const Color.fromARGB(255, 84, 109, 191)),
         useMaterial3: true,
       ),
@@ -37,9 +39,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Home Page
+
   static const platform = MethodChannel('samples.flutter.dev/spyware');
-  // initialize method channel
-  List<Map<String, dynamic>> _spywareApps = [];
+  // initialize method channel to correspond with native languages
+  List<Map<String, dynamic>> _spywareApps = []; //to store all detected spyware
 
   Future<void> _getSpywareApps() async {
     List<dynamic> spywareApps;
@@ -67,6 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  //Widget for homepage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,31 +89,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _spywareApps.length,
-        itemBuilder: (context, index) {
-          var app = _spywareApps[index];
-          return ListTile(
-            leading: app['icon'] != null
-                ? Image.memory(base64Decode(app['icon']?.trim() ?? ''))
-                : null, // Displays the icon for the app if it's not null
-            title: RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  TextSpan(
-                    text: '${app['name'] ?? 'Unknown Name'}  ',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: '(${app['id'] ?? 'Unknown ID'})',
-                  ),
-                ],
-              ),
+      body: _spywareApps.isEmpty //if list is empty, no spyware apps detected,
+          ? const Center(child: Text("No spyware apps detected on your device"))
+          : ListView.builder(
+              //otherwise, build the list view and display it.
+              itemCount: _spywareApps.length,
+              itemBuilder: (context, index) {
+                var app = _spywareApps[index];
+                return ListTile(
+                    leading: app['icon'] != null
+                        ? Image.memory(base64Decode(app['icon']?.trim() ?? ''))
+                        : null, // Displays the icon for the app if it's not null
+                    title: RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '${app['name'] ?? 'Unknown Name'}  ',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: '(${app['id'] ?? 'Unknown ID'})',
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => AppDetailPage(appData: app),
+                      ));
+                    });
+              },
             ),
-          );
-        },
-      ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
@@ -121,6 +133,40 @@ class _MyHomePageState extends State<MyHomePage> {
           ), // This button continues to initiate the scan
           child: const Text('List Detected Spyware Applications'),
         ),
+      ),
+    );
+  }
+}
+
+class AppDetailPage extends StatelessWidget {
+  final Map<String, dynamic> appData;
+
+  const AppDetailPage({super.key, required this.appData});
+
+  static const platform = MethodChannel('com.example.spyware/settings');
+
+  Future<void> _openAppSettings(String package) async {
+    try {
+      await platform.invokeMethod('openAppSettings', {'package': package});
+    } catch (e) {
+      print('Failed to open app settings: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Details'),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Your existing widgets...
+          ElevatedButton(
+            onPressed: () => _openAppSettings(appData['id']),
+            child: const Text('Delete App in App Settings'),
+          )
+        ],
       ),
     );
   }
